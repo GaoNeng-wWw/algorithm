48f829350f175c31d25a4d3c2fcceabb5bdccef5
GaoNeng-wWw <gaonenggg@outlook.com>
2023-12-14 13:24:51 +0800

    feat(data-struct): BST

---
+++ packages/data-struct/BST.ts
@@ -0,0 +1,136 @@
+export class TreeNode<T> {
+    public val: T;
+    public left: TreeNode<T> | null;
+    public right: TreeNode<T> | null;
+    public parent: TreeNode<T> | null = null;
+    constructor(
+        val?:T,
+        left: TreeNode<T> | null = null,
+        right: TreeNode<T> | null = null,
+        parent: TreeNode<T> | null = null,
+    ){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        this.parent = parent;
+    }
+    successors(){
+        let node = this.right;
+        if (node){
+            while (node.left){
+                node = node.left;
+            }
+            return node;
+        } else {
+            node = this;
+            while (node.parent && node.parent.right === node){
+                node = node.parent;
+            }
+            return node;
+        }
+    }
+    pred(){
+        let node = this.left        
+        if (node !== null){
+            while (node.right){
+                node = node.right;
+            }
+            return node;
+        } else {
+            node = this;
+            while (node.parent && node.parent.left === node){
+                node = node.parent;
+            }
+            return node.parent;
+        }
+    }
+    get isLeaf(){
+        return this.left === null && this.right === null;
+    }
+}
+export class BST<T> {
+    private root: TreeNode<T> | null = null;
+    constructor(
+        val?: T
+    ){
+        if (val){
+            this.append(val);
+        }
+    }
+    append(val: T | T[]){
+        if (Array.isArray(val)){
+            for (const item of val){
+                this.append(item);
+            }
+            return this.root;
+        }
+        const node = val instanceof TreeNode ? val : new TreeNode(val, null, null, null);
+        if (!this.root){
+            this.root = node;
+            return this.root;
+        }
+        let cur = this.root;
+        let parent = this.root;
+        while (cur){
+            parent = cur;
+            if (node.val > cur.val){
+                cur = cur.right;
+                continue;
+            }
+            cur = cur.left;
+        }
+        node.parent = parent;
+        if (node.val > parent.val){
+            parent.right = node;
+        } else{
+            parent.left = node;
+        }
+        return this.root;
+    }
+    findByVal(val:T): TreeNode<T> | null {
+        let cur = this.root;
+        while (cur){
+            if (cur.val === val){
+                return cur;
+            }
+            if (val > cur.val){
+                cur = cur.right;
+            } else {
+                cur = cur.left;
+            }
+        }
+        return cur;
+    }
+    del<E extends T>(val: E){
+        const node = val instanceof TreeNode ? val : this.findByVal(val);
+        if (node.isLeaf){
+            if (node.parent.left === node){
+                node.parent.left = null;
+            } else {
+                node.parent.right = null;
+            }
+            return this.root;
+        } else {
+            if (node.left && node.right){
+                const successors = node.successors() as TreeNode<T>;
+                node.val = successors.val;
+                this.del(successors as T);
+                return this.root;
+            } else if (node.left || node.right){
+                if (node.left){
+                    if (node.parent.right === node){
+                        node.left.parent = node.parent;
+                        node.parent.right = node.left;
+                        return this.root;
+                    }
+                } else {
+                    if (node.parent.right === node){
+                        node.right.parent = node.parent;
+                        node.parent.right = node.right;
+                        return this.root;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ packages/data-struct/__tests__/BST.spec.ts
@@ -0,0 +1,81 @@
+import { beforeEach,it,expect, describe } from "vitest";
+import { BST } from "../BST";
+let root: BST<number>;
+beforeEach(()=>{
+    root = new BST()
+})
+
+it('find', ()=>{
+    expect(root.findByVal(123)).toBe(null);
+})
+
+it('add', ()=>{
+    for (let i=100;i>0;i-=10){
+        root.append(i);
+    }
+    for (let i=100;i>0;i-=10){
+        expect(root.findByVal(i).val).toBe(i)
+    }
+})
+describe('delete', ()=>{
+    it('delete leaf', ()=>{
+        root.append(1)
+        root.append(2)
+        root.del(2);
+        expect(root.findByVal(2)).toBeNull();
+        expect(root.findByVal(1).right).toBeNull()
+    })
+    it('only left',()=>{
+        root.append(1)
+        root.append(2)
+        root.append(1.5)
+        root.del(2);
+        expect(root.findByVal(2)).toBeNull();
+        expect(root.findByVal(1.5).parent).toBe(root.findByVal(1));
+        expect(root.findByVal(1).right).toBe(root.findByVal(1.5));
+    })
+    it('only right', ()=>{
+        root.append(1)
+        root.append(2)
+        root.append(2.5)
+        root.del(2);
+        expect(root.findByVal(2)).toBeNull();
+        expect(root.findByVal(2.5).parent).toBe(root.findByVal(1));
+        expect(root.findByVal(1).right).toBe(root.findByVal(2.5));
+    })
+    it('left and right', () => {
+        const arr = [16,8,4,2,6,12,10,14,24,20,18,22,28,26,30]
+        for (const item of arr){
+            root.append(item);
+        }
+        const p = root.findByVal(24).parent;
+        root.del(24)
+        expect(root.findByVal(24)).toBeNull()
+        expect(p.right).toBe(root.findByVal(26))
+    })
+})
+
+describe('successors', ()=>{
+    it('left-skewed', ()=>{
+        const data = [10,5,15,3,8,9,20]
+        root.append(data);
+        expect(root.findByVal(20).successors().val).toBe(10)
+    })
+    it('right-skewed', ()=>{
+        const data = [10,20,30,40,50,60,70,80,90,100]
+        root.append(data);
+        expect(root.findByVal(10).successors().val).toBe(20)
+    })
+})
+describe('pred', ()=>{
+    it('left-skewed', ()=>{
+        const data = [10,5,15,3,8,9,20]
+        root.append(data);
+        expect(root.findByVal(10).pred().val).toBe(9)
+    })
+    it('right-skewed', ()=>{
+        const data = [10,5,15,3,8,9,20,7];
+        root.append(data);
+        expect(root.findByVal(9).pred().val).toBe(8)
+    })
+})
